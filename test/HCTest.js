05573be412d67ea4f34a8d45e30197f79e2ea5a2
ItsNickBarry <itsnickbarry@protonmail.com>
2015-06-17 18:40:10 -0700

    update example, add Circle.equals

--- test/HCTest.js
+++ test/HCTest.js
@@ -22,30 +22,30 @@
     var rot = 0;
     // var radius = .5;
 
-    window.q1 = Point.fromCoordinates(.5, .5);
-    window.q2 = Point.fromCoordinates(-.5, .5);
-    window.q3 = Point.fromCoordinates(-.5, -.5);
-    window.q4 = Point.fromCoordinates(.5, -.5);
+    window.q1 = Point.givenCoordinates(.5, .5);
+    window.q2 = Point.givenCoordinates(-.5, .5);
+    window.q3 = Point.givenCoordinates(-.5, -.5);
+    window.q4 = Point.givenCoordinates(.5, -.5);
 
-    window.xPlus = Point.fromCoordinates(.5, .0);
-    window.xMinus = Point.fromCoordinates(-.5, .0);
-    window.yPlus = Point.fromCoordinates(0, .5);
-    window.yMinus = Point.fromCoordinates(0, -.5);
+    window.xPlus = Point.givenCoordinates(.5, .0);
+    window.xMinus = Point.givenCoordinates(-.5, .0);
+    window.yPlus = Point.givenCoordinates(0, .5);
+    window.yMinus = Point.givenCoordinates(0, -.5);
 
 
     var radius = xPlus.distanceFromCenter() / 2;
 
-    window.polCenter = Polygon.fromNCenterRadius(sides, Point.CENTER, radius, rot)
+    window.polCenter = Polygon.givenNCenterRadius(sides, Point.CENTER, radius, rot)
 
-    window.polQ1 = Polygon.fromNCenterRadius(sides, q1, radius, rot);
-    window.polQ2 = Polygon.fromNCenterRadius(sides, q2, radius, rot);
-    window.polQ3 = Polygon.fromNCenterRadius(sides, q3, radius, rot);
-    window.polQ4 = Polygon.fromNCenterRadius(sides, q4, radius, rot);
+    window.polQ1 = Polygon.givenNCenterRadius(sides, q1, radius, rot);
+    window.polQ2 = Polygon.givenNCenterRadius(sides, q2, radius, rot);
+    window.polQ3 = Polygon.givenNCenterRadius(sides, q3, radius, rot);
+    window.polQ4 = Polygon.givenNCenterRadius(sides, q4, radius, rot);
 
-    window.polXPlus =  Polygon.fromNCenterRadius(sides, xPlus, radius, rot);
-    window.polYPlus =  Polygon.fromNCenterRadius(sides, yPlus, radius, rot);
-    window.polXMinus = Polygon.fromNCenterRadius(sides, xMinus, radius, rot);
-    window.polYMinus = Polygon.fromNCenterRadius(sides, yMinus, radius, rot);
+    window.polXPlus =  Polygon.givenNCenterRadius(sides, xPlus, radius, rot);
+    window.polYPlus =  Polygon.givenNCenterRadius(sides, yPlus, radius, rot);
+    window.polXMinus = Polygon.givenNCenterRadius(sides, xMinus, radius, rot);
+    window.polYMinus = Polygon.givenNCenterRadius(sides, yMinus, radius, rot);
 
     window.c = HyperbolicCanvas.canvases[0];
 
@@ -87,9 +87,9 @@
         xMinusMinus.x < 0 &&
         yMinusMinus.y < 0
       )
-      console.log("Same axis? " + sameAxisPass);
-      console.log("Same side of other axis? " + sameSidePass);
-      console.log("Other side of other axis? " + otherSidePass);
+      // console.log("Same axis? " + sameAxisPass);
+      // console.log("Same side of other axis? " + sameSidePass);
+      // console.log("Other side of other axis? " + otherSidePass);
     }();
 
     // c.setStrokeStyle('white');
@@ -120,22 +120,30 @@
     // c.fillPolygon(polYMinus);
     //
     // c.setStrokeStyle('black');
-    // c.strokeCircle(Circle.fromHyperbolicCenterRadius(q1, radius));
-    // c.strokeCircle(Circle.fromHyperbolicCenterRadius(q2, radius));
-    // c.strokeCircle(Circle.fromHyperbolicCenterRadius(q3, radius));
-    // c.strokeCircle(Circle.fromHyperbolicCenterRadius(q4, radius));
+    // c.strokeCircle(Circle.givenHyperbolicCenterRadius(q1, radius));
+    // c.strokeCircle(Circle.givenHyperbolicCenterRadius(q2, radius));
+    // c.strokeCircle(Circle.givenHyperbolicCenterRadius(q3, radius));
+    // c.strokeCircle(Circle.givenHyperbolicCenterRadius(q4, radius));
     //
-    // var xPlusCircle = Circle.fromHyperbolicCenterRadius(xPlus, radius);
+    // var xPlusCircle = Circle.givenHyperbolicCenterRadius(xPlus, radius);
     // c.strokeCircle(xPlusCircle);
-    // c.strokeCircle(Circle.fromHyperbolicCenterRadius(xMinus, radius));
-    // c.strokeCircle(Circle.fromHyperbolicCenterRadius(yPlus, radius));
-    // c.strokeCircle(Circle.fromHyperbolicCenterRadius(yMinus, radius));
+    // c.strokeCircle(Circle.givenHyperbolicCenterRadius(xMinus, radius));
+    // c.strokeCircle(Circle.givenHyperbolicCenterRadius(yPlus, radius));
+    // c.strokeCircle(Circle.givenHyperbolicCenterRadius(yMinus, radius));
 
     var n = 6;
     var r = 1;
-    var p = Point.fromCoordinates(.0001, .0001);
+    var p = Point.CENTER;//givenCoordinates(.0001, .0001);
     var count = 12;
     var rotation = 0;
+
+    // var color='#0F0'
+		// c.ctx.shadowColor = color;
+		// c.ctx.shadowBlur = 40;
+		// c.ctx.strokeStyle = color;
+    // c.ctx.fillStyle = color;
+		// c.ctx.lineWidth = 12;
+
     var fn = function () {
       if (!reRender) {
         return false;
@@ -144,15 +152,20 @@
       for (var i = 0; i < count; i++) {
         for (var j = 0; j < n; j++) {
           var p2 = p.distantPoint(i * r * 2, Math.TAU / n * j - rotation)
-          var gon = Polygon.fromNCenterRadius(n, p2, r, Math.TAU / n * j + Math.PI * i + rotation)
+          var gon = Polygon.givenNCenterRadius(n, p2, r, Math.TAU / n * j + Math.PI * i + rotation)
           polygons.push(gon);
         }
       }
       rotation += Math.TAU / (n * n * 2);
       c.clear();
-      polygons.forEach(c.fillPolygon.bind(c));
       polygons.forEach(function (p) {
-        // c.strokePolygonBoundaries(p)
+        // var color='#0F0'
+    		// c.ctx.shadowColor = color;
+    		// c.ctx.shadowBlur = 40;
+    		// c.ctx.strokeStyle = color;
+        // c.ctx.fillStyle = color;
+    		// c.ctx.lineWidth = 2;
+        c.fillPolygon(p);
         // c.setStrokeStyle("#" + Math.floor(Math.random() * 1000000));
       });
       // c.strokeGrid(2);
@@ -161,7 +174,7 @@
     // reRender = false;
     setInterval(fn, 200);
 
-    // var p = c.at(Point.fromCoordinates(.2, -.5))
+    // var p = c.at(Point.givenCoordinates(.2, -.5))
     // c.ctx.beginPath();
     // c.ctx.arc(c.radius,c.radius,c.radius,0,Math.TAU)
     // c.ctx.stroke();
@@ -169,11 +182,11 @@
     // c.ctx.fillText("A",p[0] + 5, p[1] + 15)
     // c.ctx.fill();
 
-    // var cent = Point.fromCoordinates(.5, -.5);
-    // var cir = Circle.fromHyperbolicCenterRadius(cent, 2);
+    // var cent = Point.givenCoordinates(.5, -.5);
+    // var cir = Circle.givenHyperbolicCenterRadius(cent, 2);
     // c.drawCircle(cir);
     // for (var i = 0; i < Math.TAU; i += (Math.TAU / 12)) {
-    //   c.strokeLine(Line.fromTwoPoints(cent, cir.pointAt(i)));
+    //   c.strokeLine(Line.givenTwoPoints(cent, cir.pointAt(i)));
     // }
   };
 })();
