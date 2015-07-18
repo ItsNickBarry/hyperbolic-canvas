cee56fdd92fbf62ca2b6eb1d5fa18bac30f138ad
ItsNickBarry <itsnickbarry@protonmail.com>
2015-07-18 01:49:10 -0400

    add hand-drawn polygon example

---
+++ example_hand_drawn_polygon.js
@@ -0,0 +1,47 @@
+;
+(function () {
+  var fn = function () {
+    var canvas = HyperbolicCanvas.canvases[0];
+    canvas.setFillStyle('#DD4814');
+
+    var vertices = [];
+
+
+    var render = function (event) {
+      canvas.clear();
+
+      var point = canvas.at([event.clientX, event.clientY]);
+
+
+      if (vertices.length >= 2) {
+        var tempVertices = [];
+        vertices.forEach(function (v) {
+          tempVertices.push(v);
+        });
+        tempVertices.push(point);
+
+        var polygon = HyperbolicCanvas.Polygon.givenVertices(tempVertices);
+        canvas.fillPolygon(polygon);
+      } else if (vertices.length == 1) {
+        var line = HyperbolicCanvas.Line.givenTwoPoints(vertices[0], point);
+        canvas.strokeLine(line);
+      }
+    };
+
+    var addVertex = function (event) {
+      var point = canvas.at([event.clientX, event.clientY]);
+      if (!(vertices.length > 0 && point.equals(vertices[vertices.length - 1]))) {
+        vertices.push(point);
+      }
+    };
+
+    canvas.canvas.addEventListener('click', addVertex);
+    canvas.canvas.addEventListener('mousemove', render);
+  };
+
+  if (document.readyState != 'loading'){
+    render();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+})();
