ad87fa7d351f1fea11f451137b2dc9581450b7e8
Nick Barry <itsnickbarry@protonmail.com>
2015-10-21 19:27:23 -0400

    bind scripts to canvas objects

--- scripts/hand-drawn-polygon.js
+++ scripts/hand-drawn-polygon.js
@@ -1,48 +1,53 @@
 ;
 (function () {
-  var fn = function () {
-    var canvas = HyperbolicCanvas.canvases['hand-drawn-polygon'];
-    canvas.setFillStyle('#DD4814');
-
-    var vertices = [];
-
-
-    var render = function (event) {
-      canvas.clear();
-
-      var point = canvas.at([event.clientX, event.clientY]);
-
-
-      if (vertices.length >= 2) {
-        var tempVertices = [];
-        vertices.forEach(function (v) {
-          tempVertices.push(v);
-        });
-        tempVertices.push(point);
-
-        var polygon = HyperbolicCanvas.Polygon.givenVertices(tempVertices);
-        canvas.fillPolygon(polygon);
-        canvas.strokePolygonBoundaries(polygon);
-      } else if (vertices.length == 1) {
-        var line = HyperbolicCanvas.Line.givenTwoPoints(vertices[0], point);
-        canvas.strokeLine(line);
-      }
-    };
-
-    var addVertex = function (event) {
-      var point = canvas.at([event.clientX, event.clientY]);
-      if (!(vertices.length > 0 && point.equals(vertices[vertices.length - 1]))) {
-        vertices.push(point);
-      }
-    };
-
-    canvas.canvas.addEventListener('click', addVertex);
-    canvas.canvas.addEventListener('mousemove', render);
-  };
-
-  if (document.readyState != 'loading'){
-    render();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
+  if (typeof HyperbolicCanvas === "undefined") {
+    window.HyperbolicCanvas = {};
   }
+  if (typeof HyperbolicCanvas.scripts === "undefined") {
+    window.HyperbolicCanvas.scripts = {};
+  }
+
+
+  HyperbolicCanvas.scripts['hand-drawn-polygon'] = function (canvas) {
+    var fn = function () {
+      canvas.setFillStyle('#DD4814');
+
+      var vertices = [];
+
+
+      var render = function (event) {
+        canvas.clear();
+
+        var point = canvas.at([event.clientX, event.clientY]);
+
+
+        if (vertices.length >= 2) {
+          var tempVertices = [];
+          vertices.forEach(function (v) {
+            tempVertices.push(v);
+          });
+          tempVertices.push(point);
+
+          var polygon = HyperbolicCanvas.Polygon.givenVertices(tempVertices);
+          canvas.fillPolygon(polygon);
+          canvas.strokePolygonBoundaries(polygon);
+        } else if (vertices.length == 1) {
+          var line = HyperbolicCanvas.Line.givenTwoPoints(vertices[0], point);
+          canvas.strokeLine(line);
+        }
+      };
+
+      var addVertex = function (event) {
+        var point = canvas.at([event.clientX, event.clientY]);
+        if (!(vertices.length > 0 && point.equals(vertices[vertices.length - 1]))) {
+          vertices.push(point);
+        }
+      };
+
+      canvas.canvas.addEventListener('click', addVertex);
+      canvas.canvas.addEventListener('mousemove', render);
+    };
+
+    fn();
+  };
 })();
--- scripts/hexagons.js
+++ scripts/hexagons.js
@@ -1,50 +1,51 @@
 ;
 (function () {
-  var reRender = true;
+  if (typeof HyperbolicCanvas === "undefined") {
+    window.HyperbolicCanvas = {};
+  }
+  if (typeof HyperbolicCanvas.scripts === "undefined") {
+    window.HyperbolicCanvas.scripts = {};
+  }
 
-  var render = function () {
+  HyperbolicCanvas.scripts['hexagons'] = function (canvas) {
+    var reRender = true;
 
-    var canvas = HyperbolicCanvas.canvases['hexagons'];
+    var render = function () {
+      canvas.setFillStyle('#DD4814');
 
-    canvas.setFillStyle('#DD4814');
+      var sideCount = 6;
+      var radius = 1;
+      var count = 12;
+      var rotation = 0;
+      var rotationInterval = Math.TAU / (sideCount * sideCount * sideCount * sideCount * 2)
 
-    var sideCount = 6;
-    var radius = 1;
-    var count = 12;
-    var rotation = 0;
-    var rotationInterval = Math.TAU / (sideCount * sideCount * sideCount * sideCount * 2)
-
-    var fn = function () {
-      if (!reRender) {
-        return false;
-      }
-      var polygons = [];
-      for (var i = 0; i < count; i++) {
-        for (var j = 0; j < sideCount; j++) {
-          var center = HyperbolicCanvas.Point.givenHyperbolicPolarCoordinates(i * radius * 2, Math.TAU / sideCount * j - rotation);
-          var gon = HyperbolicCanvas.Polygon.givenNCenterRadius(sideCount, center, radius, Math.TAU / sideCount * j + Math.PI * i + rotation);
-          polygons.push(gon);
+      var fn = function () {
+        if (!reRender) {
+          return false;
         }
-      }
-      rotation += rotationInterval;
-      canvas.clear();
-      polygons.forEach(function (polygon) {
-        canvas.fillPolygon(polygon);
-      });
+        var polygons = [];
+        for (var i = 0; i < count; i++) {
+          for (var j = 0; j < sideCount; j++) {
+            var center = HyperbolicCanvas.Point.givenHyperbolicPolarCoordinates(i * radius * 2, Math.TAU / sideCount * j - rotation);
+            var gon = HyperbolicCanvas.Polygon.givenNCenterRadius(sideCount, center, radius, Math.TAU / sideCount * j + Math.PI * i + rotation);
+            polygons.push(gon);
+          }
+        }
+        rotation += rotationInterval;
+        canvas.clear();
+        polygons.forEach(function (polygon) {
+          canvas.fillPolygon(polygon);
+        });
+      };
+      fn();
+      setInterval(fn, 100);
     };
-    fn();
-    setInterval(fn, 100);
-  };
 
-  var toggleRender = function () {
-    reRender ^= true;
-  };
+    var toggleRender = function () {
+      reRender ^= true;
+    };
 
-  document.addEventListener('click', toggleRender);
-
-  if (document.readyState != 'loading'){
+    document.addEventListener('click', toggleRender);
     render();
-  } else {
-    document.addEventListener('DOMContentLoaded', render);
   }
 })();
