88ea7dc5ba7440c0161dd727c846b84b337be346
Nick Barry <itsnickbarry@protonmail.com>
2015-10-22 01:29:43 -0400

    remove superfluous nested function and excess hexagons

--- scripts/hexagons.js
+++ scripts/hexagons.js
@@ -8,34 +8,30 @@
   }
 
   HyperbolicCanvas.scripts['hexagons'] = function (canvas) {
-    var render = function () {
-      canvas.setFillStyle('#DD4814');
+    canvas.setFillStyle('#DD4814');
 
-      var sideCount = 6;
-      var radius = 1;
-      var count = 12;
-      var rotation = 0;
-      var rotationInterval = Math.TAU / (sideCount * sideCount * sideCount * sideCount * 2)
+    var sideCount = 6;
+    var radius = 1;
+    var count = 4;
+    var rotation = 0;
+    var rotationInterval = Math.TAU / (Math.pow(sideCount, 4) * 2)
 
-      var fn = function () {
-        var polygons = [];
-        for (var i = 0; i < count; i++) {
-          for (var j = 0; j < sideCount; j++) {
-            var center = HyperbolicCanvas.Point.givenHyperbolicPolarCoordinates(i * radius * 2, Math.TAU / sideCount * j - rotation);
-            var gon = HyperbolicCanvas.Polygon.givenNCenterRadius(sideCount, center, radius, Math.TAU / sideCount * j + Math.PI * i + rotation);
-            polygons.push(gon);
-          }
+    var fn = function () {
+      var polygons = [];
+      for (var i = 0; i < count; i++) {
+        for (var j = 0; j < sideCount; j++) {
+          var center = HyperbolicCanvas.Point.givenHyperbolicPolarCoordinates(i * radius * 2, Math.TAU / sideCount * j + (i % 2 === 0 ? rotation : rotation * -1));
+          var gon = HyperbolicCanvas.Polygon.givenNCenterRadius(sideCount, center, radius, Math.TAU / sideCount * j + Math.PI * i + rotation);
+          polygons.push(gon);
         }
-        rotation += rotationInterval;
-        canvas.clear();
-        polygons.forEach(function (polygon) {
-          canvas.fillPolygon(polygon);
-        });
-      };
-      fn();
-      setInterval(fn, 100);
+      }
+      rotation += rotationInterval;
+      canvas.clear();
+      polygons.forEach(function (polygon) {
+        canvas.fillPolygon(polygon);
+      });
     };
-
-    render();
-  }
+    fn();
+    setInterval(fn, 100);
+  };
 })();
