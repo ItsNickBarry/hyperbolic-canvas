e7fcb1059849cd8ff1e2ef111a539bcec11c671c
Nick Barry <itsnickbarry@protonmail.com>
2015-10-23 16:36:41 -0400

    harmonize hexagons

--- scripts/hexagons.js
+++ scripts/hexagons.js
@@ -11,17 +11,26 @@
     canvas.setFillStyle('#DD4814');
 
     var sideCount = 6;
-    var radius = 1;
-    var count = 4;
+    var radius = .9;
+    var ringCount = 4;
     var rotation = 0;
-    var rotationInterval = Math.TAU / (Math.pow(sideCount, 4) * 2)
+    var rotationDenominator = Math.pow(sideCount, 4) * 3;
+    var rotationInterval = Math.TAU / rotationDenominator;
 
     var fn = function () {
       var polygons = [];
-      for (var i = 0; i < count; i++) {
+      for (var i = 0; i < ringCount; i++) {
         for (var j = 0; j < sideCount; j++) {
-          var center = HyperbolicCanvas.Point.givenHyperbolicPolarCoordinates(i * radius * 2, Math.TAU / sideCount * j + (i % 2 === 0 ? rotation : rotation * -1));
-          var gon = HyperbolicCanvas.Polygon.givenNCenterRadius(sideCount, center, radius, Math.TAU / sideCount * j + Math.PI * i + rotation);
+          var center = HyperbolicCanvas.Point.givenHyperbolicPolarCoordinates(
+            i * radius * 2,
+            Math.TAU / sideCount * j + (i % 2 === 0 ? rotation : rotation * -1)
+          );
+          var gon = HyperbolicCanvas.Polygon.givenNCenterRadius(
+            sideCount,
+            center,
+            radius,
+            (Math.TAU / sideCount * j + rotation) * (i + 1) // + Math.PI * i // if sideCount is odd
+          );
           polygons.push(gon);
         }
       }
