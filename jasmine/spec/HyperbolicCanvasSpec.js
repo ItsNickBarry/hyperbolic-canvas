a796b8ca7eeb8c0867e22b4b8f4192fd8319b914
Nick Barry <itsnickbarry@protonmail.ch>
2016-04-16 01:59:06 -0400

    add HyperbolicCanvas specs, properly attribute Circle.intersects

--- jasmine/spec/HyperbolicCanvasSpec.js
+++ jasmine/spec/HyperbolicCanvasSpec.js
@@ -3,5 +3,23 @@ describe('HyperbolicCanvas', function () {
     expect(Math.TAU).toEqual(Math.PI * 2);
   });
 
-  // TODO HyperbolicCanvas.create
+  describe('when creating a Canvas', function () {
+    it('should create a Canvas', function () {
+      expect(HyperbolicCanvas.create()).toBeA(HyperbolicCanvas.Canvas);
+    });
+
+    it('should store reference to unnamed Canvas', function () {
+      expect(
+        HyperbolicCanvas.canvases['canvas0']
+      ).toBeA(HyperbolicCanvas.Canvas);
+    });
+
+    it('should store reference to named Canvas', function () {
+      // create without specifying selector
+      HyperbolicCanvas.create(undefined, 'asdf');
+      expect(
+        HyperbolicCanvas.canvases['asdf']
+      ).toBeA(HyperbolicCanvas.Canvas);
+    });
+  });
 });
