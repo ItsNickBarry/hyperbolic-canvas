b4300533ffe0204871e9452b96812e1094719e9a
Nick Barry <itsnickbarry@protonmail.ch>
2024-06-27 18:22:37 +0400

    add browserify dev dependency and rebuild

--- dist/hyperbolic_canvas.js
+++ dist/hyperbolic_canvas.js
@@ -900,31 +900,33 @@
     5: [
       function (require, module, exports) {
         (function (global) {
-          const HyperbolicCanvas =
-            (module.exports =
-            global.HyperbolicCanvas =
-              {});
+          (function () {
+            const HyperbolicCanvas =
+              (module.exports =
+              global.HyperbolicCanvas =
+                {});
 
-          // modules attach themselves to HyperbolicCanvas and do not include exports
-          require('./angle.js');
-          require('./point.js');
-          require('./line.js');
-          require('./circle.js');
-          require('./polygon.js');
-          require('./canvas.js');
+            // modules attach themselves to HyperbolicCanvas and do not include exports
+            require('./angle.js');
+            require('./point.js');
+            require('./line.js');
+            require('./circle.js');
+            require('./polygon.js');
+            require('./canvas.js');
 
-          HyperbolicCanvas.create = function (selector) {
-            return new HyperbolicCanvas.Canvas({
-              el:
-                document.querySelector(selector) ||
-                document.createElement('div'),
-            });
-          };
+            HyperbolicCanvas.create = function (selector) {
+              return new HyperbolicCanvas.Canvas({
+                el:
+                  document.querySelector(selector) ||
+                  document.createElement('div'),
+              });
+            };
 
-          HyperbolicCanvas.INFINITY = 1e12;
-          HyperbolicCanvas.ZERO = 1e-6;
+            HyperbolicCanvas.INFINITY = 1e12;
+            HyperbolicCanvas.ZERO = 1e-6;
 
-          Math.TAU = 2 * Math.PI;
+            Math.TAU = 2 * Math.PI;
+          }).call(this);
         }).call(
           this,
           typeof global !== 'undefined'
